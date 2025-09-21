fd07ca291ddb8abb606643be5eb1909dbea996b0
jamiet9uni <yd24532@bristol.ac.uk>
2025-09-21 11:42:53 +0100

    Create ilos.js

---
+++ src/ilos.js
@@ -0,0 +1,63 @@
+// src/ilos.js
+
+// Array of ILO objects
+const ILOS = [
+  {
+    ilo: "Apply the general composition of connective tissue to cartilage, bones, tendons and ligaments",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Briefly describe the histological anatomy of the three main types of cartilage and provide a gross anatomy example of each",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Describe the structure of mammalian bones",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Summarise the main characteristics and main functions of osteoclasts, osteoblasts and osteocytes (bone cells)",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Describe the processes of bone mineralisation and bone development",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Outline the process of fracture repair",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Describe the three main joint types",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  },
+  {
+    ilo: "Provide an example for each of the seven classifications of synovial joints",
+    title: "Overview of the Musculoskeletal System 1",
+    type: "Lecture",
+    week: "Week 0",
+    link: "https://www.ole.bris.ac.uk/ultra/courses/_266749_1/outline/edit/document/_9707492_1?courseId=_266749_1"
+  }
+];
+
+export default ILOS;
